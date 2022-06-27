2ace8668a0c78077d622f1253232c531158db369
Manoj Varma <manojvarma1998@gmail.com>
2022-06-27 19:51:16 +0530

    Updated

--- Problem-1.py
+++ Problem-1.py
@@ -1,22 +1,26 @@
-def main():
-    cube = float(input('Enter the number : '))
-    epsilon = 0.01
-    if(cube>-1 and cube<1):
-        low = abs(cube)
-        high = 1
-    else:
-        low=0
-        high=abs(cube)
-    guess = (high + low)/2.0
-    while abs(guess**3 - abs(cube)) >= epsilon:
-        if guess**3 < abs(cube) :
-            low = guess
+try:
+    def main():
+        cube = float(input('Enter the number : '))
+        epsilon = 0.01
+        if(cube>-1 and cube<1):
+            low = abs(cube)
+            high = 1
         else:
-            high = guess
+            low=0
+            high=abs(cube)
         guess = (high + low)/2.0
-    if(cube>=0):
-        print(guess, 'is close to the cube root of', cube)
-    else:
-        print(-guess, 'is close to the cube root of',cube)
+        while abs(guess**3 - abs(cube)) >= epsilon:
+            if guess**3 < abs(cube) :
+                low = guess
+            else:
+                high = guess
+            guess = (high + low)/2.0
+        if(cube>=0):
+            print(guess, 'is close to the cube root of', cube)
+        else:
+            print(-guess, 'is close to the cube root of',cube)
         
-main()
+    main()
+
+except:
+    print("Enter valid number")
